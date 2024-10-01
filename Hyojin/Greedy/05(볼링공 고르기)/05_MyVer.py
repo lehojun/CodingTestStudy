6f006b14c35354dec5a3f9f4101ad227fb71aa39
hyojin425 <gywls5184@gmail.com>
2024-10-01 21:36:36 +0900

    Greedy: 05. 볼링공 고르기 README.md 수정

--- Hyojin/Greedy/05(볼링공 고르기)/05_MyVer.py
+++ Hyojin/Greedy/05(볼링공 고르기)/05_MyVer.py
@@ -3,9 +3,8 @@ arr = list(map(int, input().split()))
 
 result = 0
 for i in range(n):
-    cur = arr[i]
     for j in range(i + 1,n):
-        if cur != arr[j]:
+        if arr[i] != arr[j]:
             result += 1
             
             
