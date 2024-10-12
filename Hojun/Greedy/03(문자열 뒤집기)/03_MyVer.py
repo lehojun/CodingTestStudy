7ad4bf9b5f9d9c85105eb81e36d947b7dd61bcab
lehojun <151193867+lehojun@users.noreply.github.com>
2024-10-12 15:18:03 +0900

    03_MyVer.py 수정

--- Hojun/Greedy/03(문자열 뒤집기)/03_MyVer.py
+++ Hojun/Greedy/03(문자열 뒤집기)/03_MyVer.py
@@ -1,8 +1,13 @@
 n = list(map(int,input()))
 result = 0
 
-for i in range(len(n)-1):
-  if n[i] == 0 and n[i+1] == 1:
+if n[0] == 0:
+  for i in range(len(n)-1):
+    if n[i] == 0 and n[i+1] == 1:
+      result +=1
+else:
+  for i in range(len(n)-1):
+    if n[i] == 1 and n[i+1] == 0:
       result +=1
 
 print(result)
